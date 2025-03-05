1f0464701406f2e76f48f50f3e358862da09086f
nandaa11109 <amfernanda0907@gmail.com>
2025-03-05 17:47:45 +0700

    Update ListProduct.jsx

--- src/Component/ListProduct.jsx
+++ src/Component/ListProduct.jsx
@@ -3,126 +3,126 @@ export const ListProduct = [
         id:1,
         brand: "Nike",
         image: "/assets/nike (1).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sepatu Nike dengan desain nyaman dan cocok untuk berbagai aktivitas sehari-hari.",
         price: "2.000.000"
     },
     {
         id:2,
         brand: "Nike",
         image: "/assets/nike (2).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sneakers Nike stylish dengan bahan berkualitas tinggi untuk tampilan sporty.",
         price: "2.500.000"
     },
     {
         id:3,
         brand: "Nike",
         image: "/assets/nike (3).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sepatu casual Nike yang ringan dan nyaman dipakai sepanjang hari.",
         price: "3.000.000"
     },
     {
         id:4,
         brand: "Nike",
         image: "/assets/nike (4).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sepatu lari Nike dengan desain aerodinamis dan daya cengkeram tinggi.",
         price: "1.000.000"
     },
     {
         id:5,
         brand: "Nike",
         image: "/assets/nike (5).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sneakers Nike dengan kombinasi warna menarik dan sol empuk.",
         price: "1.500.000"
     },
     {
         id:6,
         brand: "Nike",
         image: "/assets/nike (6).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sepatu olahraga Nike yang memberikan kenyamanan dan fleksibilitas maksimal.",
         price: "3.500.000"
     },
     {
         id:7,
         brand: "Adidas",
         image: "/assets/adidas (1).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sepatu Adidas dengan teknologi bantalan modern untuk kenyamanan ekstra.",
         price: "3.500.000"
     },
     {
         id:8,
         brand: "Adidas",
         image: "/assets/adidas (2).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sneakers Adidas klasik dengan desain timeless dan bahan premium.",
         price: "2.500.000"
     },
     {
         id:9,
         brand: "Adidas",
         image: "/assets/adidas (3).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sepatu Adidas yang nyaman dipakai untuk aktivitas sehari-hari.",
         price: "2.900.000"
     },
     {
         id:10,
         brand: "Adidas",
         image: "/assets/adidas (4).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sneakers Adidas dengan desain modern dan sol yang ringan.",
         price: "2.900.000"
     },
     {
         id:11,
         brand: "Adidas",
         image: "/assets/adidas (5).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sepatu Adidas dengan tampilan sporty dan daya tahan tinggi.",
         price: "2.900.000"
     },
     {
         id:12,
         brand: "Adidas",
         image: "/assets/adidas (6).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sepatu Adidas yang cocok untuk berbagai aktivitas dengan desain trendy.",
         price: "2.900.000"
     },
     {
         id:13,
         brand: "Puma",
         image: "/assets/puma (1).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sneakers Puma dengan desain minimalis dan kenyamanan luar biasa.",
         price: "2.900.000"
     },
     {
         id:14,
         brand: "Puma",
         image: "/assets/puma (2).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sepatu Puma casual yang cocok untuk tampilan santai dan stylish.",
         price: "2.900.000"
     },
     {
         id:15,
         brand: "Puma",
         image: "/assets/puma (3).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sneakers Puma dengan desain modern dan sol empuk untuk kenyamanan maksimal.",
         price: "2.900.000"
     },
     {
         id:16,
         brand: "Puma",
         image: "/assets/puma (4).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sepatu Puma dengan tampilan sporty dan daya cengkeram yang baik.",
         price: "2.900.000"
     },
     {
         id:17,
         brand: "Puma",
         image: "/assets/puma (5).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sepatu Puma yang ringan dan nyaman untuk digunakan sepanjang hari.",
         price: "2.900.000"
     },
     {
         id:18,
         brand: "Puma",
         image: "/assets/puma (6).jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore blanditiis corporis, alias modi voluptas.",
+        description: "Sneakers Puma dengan kombinasi warna menarik dan desain sporty.",
         price: "2.900.000"
-    },
-]
+    }
+];
